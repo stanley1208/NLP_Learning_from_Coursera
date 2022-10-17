7546f79046643040849c6ba2ed9b0a057199636f
A8250092 <stanley.mei08@gmail.com>
2022-10-17 16:10:09 +0800

    update files

--- week4/Naive Machine Translation and LSH.py
+++ week4/Naive Machine Translation and LSH.py
@@ -160,3 +160,50 @@ Y = np.random.rand(m, n) * .1
 R = np.random.rand(n, n)
 gradient = compute_gradient(X, Y, R)
 print(f"First row of the gradient matrix: {gradient[0]}")
+
+
+# UNQ_C5 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+def align_embeddings(X, Y, train_steps=100, learning_rate=0.0003, verbose=True, compute_loss=compute_loss, compute_gradient=compute_gradient):
+    '''
+    Inputs:
+        X: a matrix of dimension (m,n) where the columns are the English embeddings.
+        Y: a matrix of dimension (m,n) where the columns correspong to the French embeddings.
+        train_steps: positive int - describes how many steps will gradient descent algorithm do.
+        learning_rate: positive float - describes how big steps will  gradient descent algorithm do.
+    Outputs:
+        R: a matrix of dimension (n,n) - the projection matrix that minimizes the F norm ||X R -Y||^2
+    '''
+    np.random.seed(129)
+
+    # the number of columns in X is the number of dimensions for a word vector (e.g. 300)
+    # R is a square matrix with length equal to the number of dimensions in th  word embedding
+    R = np.random.rand(X.shape[1], X.shape[1])
+
+    for i in range(train_steps):
+        if verbose and i % 25 == 0:
+            print(f"loss at iteration {i} is: {compute_loss(X, Y, R):.4f}")
+        ### START CODE HERE ###
+        # use the function that you defined to compute the gradient
+        gradient = compute_gradient(X,Y,R)
+
+        # update R by subtracting the learning rate times gradient
+        R -= learning_rate*gradient
+        ### END CODE HERE ###
+    return R
+
+
+# UNQ_C6 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# You do not have to input any code in this cell, but it is relevant to grading, so please do not change anything
+
+# Testing your implementation.
+np.random.seed(129)
+m = 10
+n = 5
+X = np.random.rand(m, n)
+Y = np.random.rand(m, n) * .1
+R = align_embeddings(X, Y)
+
+
+# UNQ_C7 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# You do not have to input any code in this cell, but it is relevant to grading, so please do not change anything
+R_train = align_embeddings(X_train, Y_train, train_steps=400, learning_rate=0.8)
