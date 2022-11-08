dff9b698632b9850f38535ab14393fac0b9d04a4
A8250092 <stanley.mei08@gmail.com>
2022-11-08 15:24:32 +0800

    update files

--- week4/Naive Machine Translation and LSH.py
+++ week4/Naive Machine Translation and LSH.py
@@ -531,3 +531,102 @@ def create_hash_id_tables(n_universes):
 
 
 hash_tables, id_tables = create_hash_id_tables(N_UNIVERSES)
+
+
+# UNQ_C21 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# This is the code used to do the fast nearest neighbor search. Feel free to go over it
+def approximate_knn(doc_id, v, planes_l, hash_tables, id_tables, k=1, num_universes_to_use=25,
+                    hash_value_of_vector=hash_value_of_vector):
+    """Search for k-NN using hashes."""
+    # assert num_universes_to_use <= N_UNIVERSES
+
+    # Vectors that will be checked as possible nearest neighbor
+    vecs_to_consider_l = list()
+
+    # list of document IDs
+    ids_to_consider_l = list()
+
+    # create a set for ids to consider, for faster checking if a document ID already exists in the set
+    ids_to_consider_set = set()
+
+    # loop through the universes of planes
+    for universe_id in range(num_universes_to_use):
+
+        # get the set of planes from the planes_l list, for this particular universe_id
+        planes = planes_l[universe_id]
+
+        # get the hash value of the vector for this set of planes
+        hash_value = hash_value_of_vector(v, planes)
+
+        # get the hash table for this particular universe_id
+        hash_table = hash_tables[universe_id]
+
+        # get the list of document vectors for this hash table, where the key is the hash_value
+        document_vectors_l = hash_table[hash_value]
+
+        # get the id_table for this particular universe_id
+        id_table = id_tables[universe_id]
+
+        # get the subset of documents to consider as nearest neighbors from this id_table dictionary
+        new_ids_to_consider = id_table[hash_value]
+
+        ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
+
+        # loop through the subset of document vectors to consider
+        for i, new_id in enumerate(new_ids_to_consider):
+
+            if doc_id == new_id:
+                continue
+
+            # if the document ID is not yet in the set ids_to_consider...
+            if new_id not in ids_to_consider_set:
+                # access document_vectors_l list at index i to get the embedding
+                # then append it to the list of vectors to consider as possible nearest neighbors
+                document_vector_at_i = document_vectors_l[i]
+                vecs_to_consider_l.append(document_vector_at_i)
+
+                # append the new_id (the index for the document) to the list of ids to consider
+                ids_to_consider_l.append(new_id)
+
+                # also add the new_id to the set of ids to consider
+                # (use this to check if new_id is not already in the IDs to consider)
+                ids_to_consider_set.add(new_id)
+
+        ### END CODE HERE ###
+
+    # Now run k-NN on the smaller set of vecs-to-consider.
+    print("Fast considering %d vecs" % len(vecs_to_consider_l))
+
+    # convert the vecs to consider set to a list, then to a numpy array
+    vecs_to_consider_arr = np.array(vecs_to_consider_l)
+
+    # call nearest neighbors on the reduced list of candidate vectors
+    nearest_neighbor_idx_l = nearest_neighbor(v, vecs_to_consider_arr, k=k)
+
+    # Use the nearest neighbor index list as indices into the ids to consider
+    # create a list of nearest neighbors by the document ids
+    nearest_neighbor_ids = [ids_to_consider_l[idx]
+                            for idx in nearest_neighbor_idx_l]
+
+    return nearest_neighbor_ids
+
+
+#document_vecs, ind2Tweet
+doc_id = 0
+doc_to_search = all_tweets[doc_id]
+vec_to_search = document_vecs[doc_id]
+
+# UNQ_C22 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# You do not have to input any code in this cell, but it is relevant to grading, so please do not change anything
+
+# Sample
+nearest_neighbor_ids = approximate_knn(
+    doc_id, vec_to_search, planes_l, hash_tables, id_tables, k=3, num_universes_to_use=5)
+
+print(f"Nearest neighbors for document {doc_id}")
+print(f"Document contents: {doc_to_search}")
+print("")
+
+for neighbor_id in nearest_neighbor_ids:
+    print(f"Nearest neighbor at document id {neighbor_id}")
+    print(f"document contents: {all_tweets[neighbor_id]}")
