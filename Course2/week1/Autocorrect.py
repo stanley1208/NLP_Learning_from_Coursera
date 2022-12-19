ab11d062a56bba9f39b238ef2ddb4f366319de8c
A8250092 <stanley.mei08@gmail.com>
2022-12-19 18:06:08 +0800

    update files

--- Course2/week1/Autocorrect.py
+++ Course2/week1/Autocorrect.py
@@ -3,7 +3,7 @@ from collections import Counter
 import numpy as np
 import pandas as pd
 import os
-import w1_unittest
+
 
 
 # UNQ_C1 GRADED FUNCTION: process_data
@@ -35,3 +35,54 @@ word_l = process_data('./data/shakespeare.txt')
 vocab = set(word_l)  # this will be your new vocabulary
 print(f"The first ten words in the text are: \n{word_l[0:10]}")
 print(f"There are {len(vocab)} unique words in the vocabulary.")
+
+
+# UNIT TEST COMMENT: Candidate for Table Driven Tests
+# UNQ_C2 GRADED FUNCTION: get_count
+def get_count(word_l):
+    '''
+    Input:
+        word_l: a set of words representing the corpus.
+    Output:
+        word_count_dict: The wordcount dictionary where key is the word and value is its frequency.
+    '''
+
+    word_count_dict = {}  # fill this with word counts
+    ### START CODE HERE
+    word_count_dict=Counter()
+    for word in word_l:
+        word_count_dict[word]+=1
+    ### END CODE HERE ###
+    return word_count_dict
+
+
+#DO NOT MODIFY THIS CELL
+word_count_dict = get_count(word_l)
+print(f"There are {len(word_count_dict)} key values pairs")
+print(f"The count for the word 'thee' is {word_count_dict.get('thee',0)}")
+
+
+# UNQ_C3 GRADED FUNCTION: get_probs
+def get_probs(word_count_dict):
+    '''
+    Input:
+        word_count_dict: The wordcount dictionary where key is the word and value is its frequency.
+    Output:
+        probs: A dictionary where keys are the words and the values are the probability that a word will occur.
+    '''
+    probs = {}  # return this variable correctly
+
+    ### START CODE HERE ###
+    total=sum(word_count_dict.values())
+    # get the total count of words for all words in the dictionary
+    for word,count in word_count_dict.items():
+        probs[word]=count/total
+
+    ### END CODE HERE ###
+    return probs
+
+
+#DO NOT MODIFY THIS CELL
+probs = get_probs(word_count_dict)
+print(f"Length of probs is {len(probs)}")
+print(f"P('thee') is {probs['thee']:.4f}")
