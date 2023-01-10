0192e0a611335f33985656195abb6364c6ef6caa
A8250092 <stanley.mei08@gmail.com>
2023-01-10 15:02:44 +0800

    update files

--- Course2/week1/Autocorrect.py
+++ Course2/week1/Autocorrect.py
@@ -339,4 +339,86 @@ print(f"data type of corrections {type(tmp_corrections)}")
 
 
 # Test your function
-w1_unittest.test_get_corrections(get_corrections, probs, vocab)
+# w1_unittest.test_get_corrections(get_corrections, probs, vocab)
+
+
+# UNQ_C11 GRADED FUNCTION: min_edit_distance
+def min_edit_distance(source, target, ins_cost=1, del_cost=1, rep_cost=2):
+    '''
+    Input:
+        source: a string corresponding to the string you are starting with
+        target: a string corresponding to the string you want to end with
+        ins_cost: an integer setting the insert cost
+        del_cost: an integer setting the delete cost
+        rep_cost: an integer setting the replace cost
+    Output:
+        D: a matrix of len(source)+1 by len(target)+1 containing minimum edit distances
+        med: the minimum edit distance (med) required to convert the source string to the target
+    '''
+    # use deletion and insert cost as  1
+    m = len(source)
+    n = len(target)
+    # initialize cost matrix with zeros and dimensions (m+1,n+1)
+    D = np.zeros((m + 1, n + 1), dtype=int)
+
+    ### START CODE HERE (Replace instances of 'None' with your code) ###
+
+    # Fill in column 0, from row 1 to row m, both inclusive
+    for row in range(1, m+1):  # Replace None with the proper range
+        D[row, 0] = D[row-1,0]+del_cost
+
+    # Fill in row 0, for all columns from 1 to n, both inclusive
+    for col in range(1, n+1):  # Replace None with the proper range
+        D[0, col] = D[0,col-1]+ins_cost
+
+    # Loop through row 1 to row m, both inclusive
+    for row in range(1, m+1):
+
+        # Loop through column 1 to column n, both inclusive
+        for col in range(1, n+1):
+
+            # Intialize r_cost to the 'replace' cost that is passed into this function
+            r_cost = rep_cost
+
+            # Check to see if source character at the previous row
+            # matches the target character at the previous column,
+            if source[row-1]==target[col-1]:  # Replace None with a proper comparison
+                # Update the replacement cost to 0 if source and target are the same
+                r_cost = 0
+
+            # Update the cost at row, col based on previous entries in the cost matrix
+            # Refer to the equation calculate for D[i,j] (the minimum of three calculated costs)
+            D[row, col] = min(D[row-1,col]+del_cost,D[row,col-1]+ins_cost,D[row-1,col-1]+r_cost)
+
+    # Set the minimum edit distance with the cost found at row m, column n
+    med = D[m, n]
+
+    ### END CODE HERE ###
+    return D, med
+
+
+#DO NOT MODIFY THIS CELL
+# testing your implementation
+source = 'play'
+target = 'stay'
+matrix, min_edits = min_edit_distance(source, target)
+print("minimum edits: ",min_edits, "\n")
+idx = list('#' + source)
+cols = list('#' + target)
+df = pd.DataFrame(matrix, index=idx, columns= cols)
+print(df)
+
+print("-----------------------")
+
+#DO NOT MODIFY THIS CELL
+# testing your implementation
+source =  'eer'
+target = 'near'
+matrix, min_edits = min_edit_distance(source, target)
+print("minimum edits: ",min_edits, "\n")
+idx = list(source)
+idx.insert(0, '#')
+cols = list(target)
+cols.insert(0, '#')
+df = pd.DataFrame(matrix, index=idx, columns= cols)
+print(df)
