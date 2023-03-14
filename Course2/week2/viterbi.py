e9bea76100d43231e3dbb612662fa827b493d938
A8250092 <stanley.mei08@gmail.com>
2023-03-14 16:29:07 +0800

    Update viterbi.py

--- Course2/week2/viterbi.py
+++ Course2/week2/viterbi.py
@@ -4,9 +4,6 @@ from collections import defaultdict
 import math
 import numpy as np
 
-
-
-
 # load in the training corpus
 with open("./data/WSJ_02-21.pos","r") as f:
     training_corpus=f.readlines()
@@ -56,4 +53,72 @@ print("This is a sample of the test_corpus:" )
 print(prep[0:10])
 
 
+# UNQ_C1 GRADED FUNCTION: create_dictionaries
+def create_dictionaries(training_corpus, vocab, verbose=True):
+    """
+    Input:
+        training_corpus: a corpus where each line has a word followed by its tag.
+        vocab: a dictionary where keys are words in vocabulary and value is an index
+    Output:
+        emission_counts: a dictionary where the keys are (tag, word) and the values are the counts
+        transition_counts: a dictionary where the keys are (prev_tag, tag) and the values are the counts
+        tag_counts: a dictionary where the keys are the tags and the values are the counts
+    """
 
+    # initialize the dictionaries using defaultdict
+    emission_counts = defaultdict(int)
+    transition_counts = defaultdict(int)
+    tag_counts = defaultdict(int)
+
+    # Initialize "prev_tag" (previous tag) with the start state, denoted by '--s--'
+    prev_tag = '--s--'
+
+    # use 'i' to track the line number in the corpus
+    i = 0
+
+    # Each item in the training corpus contains a word and its POS tag
+    # Go through each word and its tag in the training corpus
+    for word_tag in training_corpus:
+
+        # Increment the word_tag count
+        i += 1
+
+        # Every 50,000 words, print the word count
+        if i % 50000 == 0 and verbose:
+            print(f"word count = {i}")
+
+        ### START CODE HERE ###
+        # get the word and tag using the get_word_tag helper function (imported from utils_pos.py)
+        # the function is defined as: get_word_tag(line, vocab)
+        word, tag = get_word_tag(word_tag,vocab)
+
+        # Increment the transition count for the previous word and tag
+        transition_counts[(prev_tag, tag)] += 1
+
+        # Increment the emission count for the tag and word
+        emission_counts[(tag, word)] += 1
+
+        # Increment the tag count
+        tag_counts[tag] += 1
+
+        # Set the previous tag to this tag (for the next iteration of the loop)
+        prev_tag = tag
+
+        ### END CODE HERE ###
+
+    return emission_counts, transition_counts, tag_counts
+
+
+
+emission_counts, transition_counts, tag_counts = create_dictionaries(training_corpus, vocab)
+
+# get all the POS states
+states = sorted(tag_counts.keys())
+print(f"Number of POS tags (number of 'states'): {len(states)}")
+print("View these POS tags (states)")
+print(states)
+
+
+# print(transition_counts)
+# print(emission_counts)
+# print(tag_counts)
