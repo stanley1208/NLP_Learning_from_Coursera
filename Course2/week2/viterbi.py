cd4ddbaeca960d30622fc31e4507be8391d8cfaf
A8250092 <stanley.mei08@gmail.com>
2023-03-20 16:23:55 +0800

    Update viterbi.py

--- Course2/week2/viterbi.py
+++ Course2/week2/viterbi.py
@@ -122,3 +122,98 @@ print(states)
 # print(transition_counts)
 # print(emission_counts)
 # print(tag_counts)
+
+
+print("transition examples: ")
+for ex in list(transition_counts.items())[:3]:
+    print(ex)
+print()
+
+print("emission examples: ")
+for ex in list(emission_counts.items())[200:203]:
+    print (ex)
+print()
+
+print("ambiguous word example: ")
+for tup,cnt in emission_counts.items():
+    if tup[1] == 'back': print (tup, cnt)
+
+
+# UNQ_C2 GRADED FUNCTION: predict_pos
+def predict_pos(prep, y, emission_counts, vocab, states):
+    '''
+    Input:
+        prep: a preprocessed version of 'y'. A list with the 'word' component of the tuples.
+        y: a corpus composed of a list of tuples where each tuple consists of (word, POS)
+        emission_counts: a dictionary where the keys are (tag,word) tuples and the value is the count
+        vocab: a dictionary where keys are words in vocabulary and value is an index
+        states: a sorted list of all possible tags for this assignment
+    Output:
+        accuracy: Number of times you classified a word correctly
+    '''
+
+    # Initialize the number of correct predictions to zero
+    num_correct = 0
+
+    # Get the (tag, word) tuples, stored as a set
+    all_words = set(emission_counts.keys())
+
+    # Get the number of (word, POS) tuples in the corpus 'y'
+    total = len(y)
+    for word, y_tup in zip(prep, y):
+
+        # Split the (word, POS) string into a list of two items
+        y_tup_l = y_tup.split()
+
+        # Verify that y_tup contain both word and POS
+        if len(y_tup_l) == 2:
+
+            # Set the true POS label for this word
+            true_label = y_tup_l[1]
+
+        else:
+            # If the y_tup didn't contain word and POS, go to next word
+            continue
+
+        count_final = 0
+        pos_final = ''
+
+        # If the word is in the vocabulary...
+        if word in vocab:
+            for pos in states:
+
+                ### START CODE HERE (Replace instances of 'None' with your code) ###
+
+                # define the key as the tuple containing the POS and word
+                key = (pos,word)
+
+                # check if the (pos, word) key exists in the emission_counts dictionary
+                if key in emission_counts:  # Replace None in this line with the proper condition.
+
+                    # get the emission count of the (pos,word) tuple
+                    count = emission_counts[key]
+
+                    # keep track of the POS with the largest count
+                    if count>count_final:  # Replace None in this line with the proper condition.
+
+                        # update the final count (largest count)
+                        count_final = count
+
+                        # update the final POS
+                        pos_final = pos
+
+            # If the final POS (with the largest count) matches the true POS:
+            if pos_final==true_label:  # Replace None in this line with the proper condition.
+                # Update the number of correct predictions
+                num_correct += 1
+
+    ### END CODE HERE ###
+    accuracy = num_correct / total
+
+    return accuracy
+
+
+accuracy_predict_pos = predict_pos(prep, y, emission_counts, vocab, states)
+print(f"Accuracy of prediction using predict_pos is {accuracy_predict_pos:.4f}")
+
+
