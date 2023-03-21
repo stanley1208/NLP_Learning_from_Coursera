d5a42001402049fd527c5da048b4deaa2e14b995
A8250092 <stanley.mei08@gmail.com>
2023-03-21 16:22:17 +0800

    Update viterbi.py

--- Course2/week2/viterbi.py
+++ Course2/week2/viterbi.py
@@ -217,3 +217,69 @@ accuracy_predict_pos = predict_pos(prep, y, emission_counts, vocab, states)
 print(f"Accuracy of prediction using predict_pos is {accuracy_predict_pos:.4f}")
 
 
+# UNQ_C3 GRADED FUNCTION: create_transition_matrix
+def create_transition_matrix(alpha, tag_counts, transition_counts):
+    '''
+    Input:
+        alpha: number used for smoothing
+        tag_counts: a dictionary mapping each tag to its respective count
+        transition_counts: a dictionary where the keys are (prev_tag, tag) and the values are the counts
+    Output:
+        A: matrix of dimension (num_tags,num_tags)
+    '''
+    # Get a sorted list of unique POS tags
+    all_tags=sorted(tag_counts.keys())
+
+    # Count the number of unique POS tags
+    num_tags = len(all_tags)
+
+    # Initialize the transition matrix 'A'
+    A = np.zeros((num_tags, num_tags))
+
+    # Get the unique transition tuples (previous POS, current POS)
+    trans_keys = set(transition_counts.keys())
+
+    ### START CODE HERE ###
+
+    # Go through each row of the transition matrix A
+    for i in range(num_tags):
+
+        # Go through each column of the transition matrix A
+        for j in range(num_tags):
+
+            # Initialize the count of the (prev POS, current POS) to zero
+            count = 0
+
+            # Define the tuple (prev POS, current POS)
+            # Get the tag at position i and tag at position j (from the all_tags list)
+            key = (all_tags[i],all_tags[j])  # tuple of form (tag,tag)
+
+            # Check if the (prev POS, current POS) tuple
+            # exists in the transition counts dictionary
+            if key in transition_counts:  # Replace None in this line with the proper condition.
+
+                # Get count from the transition_counts dictionary
+                # for the (prev POS, current POS) tuple
+                count = transition_counts[key]
+
+                # Get the count of the previous tag (index position i) from tag_counts
+            count_prev_tag = tag_counts[all_tags[i]]
+
+            # Apply smoothing using count of the tuple, alpha,
+            # count of previous tag, alpha, and total number of tags
+            A[i, j] = (count+alpha)/(count_prev_tag+alpha*num_tags)
+
+    ### END CODE HERE ###
+    return A
+
+
+
+alpha = 0.001
+A = create_transition_matrix(alpha, tag_counts, transition_counts)
+# Testing your function
+print(f"A at row 0, col 0: {A[0,0]:.9f}")
+print(f"A at row 3, col 1: {A[3,1]:.4f}")
+
+print("View a subset of transition matrix A")
+A_sub = pd.DataFrame(A[30:35,30:35], index=states[30:35], columns = states[30:35] )
+print(A_sub)
