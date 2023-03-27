e147c97e7c41b6512eda320a5383ed5d9c74b6c6
A8250092 <stanley.mei08@gmail.com>
2023-03-27 22:22:45 +0800

    Update viterbi.py

--- Course2/week2/viterbi.py
+++ Course2/week2/viterbi.py
@@ -283,3 +283,90 @@ print(f"A at row 3, col 1: {A[3,1]:.4f}")
 print("View a subset of transition matrix A")
 A_sub = pd.DataFrame(A[30:35,30:35], index=states[30:35], columns = states[30:35] )
 print(A_sub)
+
+
+# UNQ_C4 GRADED FUNCTION: create_emission_matrix
+
+def create_emission_matrix(alpha, tag_counts, emission_counts, vocab):
+    '''
+    Input:
+        alpha: tuning parameter used in smoothing
+        tag_counts: a dictionary mapping each tag to its respective count
+        emission_counts: a dictionary where the keys are (tag, word) and the values are the counts
+        vocab: a dictionary where keys are words in vocabulary and value is an index.
+               within the function it'll be treated as a list
+    Output:
+        B: a matrix of dimension (num_tags, len(vocab))
+    '''
+
+    # get the number of POS tag
+    num_tags = len(tag_counts)
+
+    # Get a list of all POS tags
+    all_tags = sorted(tag_counts.keys())
+
+    # Get the total number of unique words in the vocabulary
+    num_words = len(vocab)
+
+    # Initialize the emission matrix B with places for
+    # tags in the rows and words in the columns
+    B = np.zeros((num_tags, num_words))
+
+    # Get a set of all (POS, word) tuples
+    # from the keys of the emission_counts dictionary
+    emis_keys = set(list(emission_counts.keys()))
+
+    ### START CODE HERE (Replace instances of 'None' with your code) ###
+
+    # Go through each row (POS tags)
+    for i in range(num_tags):  # Replace None in this line with the proper range.
+
+        # Go through each column (words)
+        for j in range(num_words):  # Replace None in this line with the proper range.
+
+            # Initialize the emission count for the (POS tag, word) to zero
+            count = 0
+
+            # Define the (POS tag, word) tuple for this row and column
+            key = (all_tags[i],vocab[j])  # tuple of form (tag,word)
+
+            # check if the (POS tag, word) tuple exists as a key in emission counts
+            if key in emission_counts:  # Replace None in this line with the proper condition.
+
+                # Get the count of (POS tag, word) from the emission_counts d
+                count = emission_counts[key]
+
+            # Get the count of the POS tag
+            count_tag = tag_counts[all_tags[i]]
+
+            # Apply smoothing and store the smoothed value
+            # into the emission matrix B for this row and column
+            B[i, j] = (count+alpha)/(count_tag+alpha*num_words)
+
+    ### END CODE HERE ###
+    return B
+
+
+
+# creating your emission probability matrix. this takes a few minutes to run.
+alpha = 0.001
+B = create_emission_matrix(alpha, tag_counts, emission_counts, list(vocab))
+
+print(f"View Matrix position at row 0, column 0: {B[0,0]:.9f}")
+print(f"View Matrix position at row 3, column 1: {B[3,1]:.9f}")
+
+# Try viewing emissions for a few words in a sample dataframe
+cidx  = ['725','adroitly','engineers', 'promoted', 'synergy']
+
+# Get the integer ID for each word
+cols = [vocab[a] for a in cidx]
+
+# Choose POS tags to show in a sample dataframe
+rvals =['CD','NN','NNS', 'VB','RB','RP']
+
+# For each POS tag, get the row number from the 'states' list
+rows = [states.index(a) for a in rvals]
+
+# Get the emissions for the sample of words, and the sample of POS tags
+B_sub = pd.DataFrame(B[np.ix_(rows,cols)], index=rvals, columns = cidx )
+print(B_sub)
