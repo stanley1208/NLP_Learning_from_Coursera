ea42c6ddbdb7c78cce1ee59adbfc3392d03e2317
A8250092 <stanley.mei08@gmail.com>
2023-04-05 22:41:21 +0800

    Update viterbi.py

--- Course2/week2/viterbi.py
+++ Course2/week2/viterbi.py
@@ -422,3 +422,77 @@ def initialize(states, tag_counts, A, B, corpus, vocab):
 
 
 best_probs, best_paths = initialize(states, tag_counts, A, B, prep, vocab)
+
+
+# UNQ_C6 GRADED FUNCTION: viterbi_forward
+def viterbi_forward(A, B, test_corpus, best_probs, best_paths, vocab, verbose=True):
+    '''
+    Input:
+        A, B: The transition and emission matrices respectively
+        test_corpus: a list containing a preprocessed corpus
+        best_probs: an initilized matrix of dimension (num_tags, len(corpus))
+        best_paths: an initilized matrix of dimension (num_tags, len(corpus))
+        vocab: a dictionary where keys are words in vocabulary and value is an index
+    Output:
+        best_probs: a completed matrix of dimension (num_tags, len(corpus))
+        best_paths: a completed matrix of dimension (num_tags, len(corpus))
+    '''
+    # Get the number of unique POS tags (which is the num of rows in best_probs)
+    num_tags = best_probs.shape[0]
+
+    # Go through every word in the corpus starting from word 1
+    # Recall that word 0 was initialized in `initialize()`
+    for i in range(1, len(test_corpus)):
+
+        # Print number of words processed, every 5000 words
+        if i % 5000 == 0 and verbose:
+            print("Words processed: {:>8}".format(i))
+
+        ### START CODE HERE  (Replace instances of 'None' with your code EXCEPT the first 'best_path_i = None')  ###
+        # For each unique POS tag that the current word can be
+        for j in range(num_tags):  # Replace None in this line with the proper range. # for every pos tag
+
+            # Initialize best_prob for word i to negative infinity
+            best_prob_i = float("-inf")
+
+            # Initialize best_path for current word i to None
+            best_path_i = None  # Do not replace this None # @KEEPTHIS
+
+            # For each POS tag that the previous word can be:
+            for k in range(num_tags):  # Replace None in this line with the proper range.
+
+                # Calculate the probability = None
+                # best probs of POS tag k, previous word i-1 +
+                # log(prob of transition from POS k to POS j) +
+                # log(prob that emission of POS j is word i)
+                prob = best_probs[k,i-1]+np.log(A[k,j])+np.log(B[j,vocab[test_corpus[i]]])
+
+                # check if this path's probability is greater than
+                # the best probability up to and before this point
+                if prob>best_prob_i:  # Replace None in this line with the proper condition.
+
+                    # Keep track of the best probability
+                    best_prob_i = prob
+
+                    # keep track of the POS tag of the previous word
+                    # that is part of the best path.
+                    # Save the index (integer) associated with
+                    # that previous word's POS tag
+                    best_path_i = k
+
+            # Save the best probability for the
+            # given current word's POS tag
+            # and the position of the current word inside the corpus
+            best_probs[j, i] = best_prob_i
+
+            # Save the unique integer ID of the previous POS tag
+            # into best_paths matrix, for the POS tag of the current word
+            # and the position of the current word inside the corpus.
+            best_paths[j, i] = best_path_i
+
+        ### END CODE HERE ###
+    return best_probs, best_paths
+
+
+# this will take a few minutes to run => processes ~ 30,000 words
+best_probs, best_paths = viterbi_forward(A, B, prep, best_probs, best_paths, vocab)
