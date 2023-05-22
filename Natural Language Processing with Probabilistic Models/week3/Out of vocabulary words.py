f8048b56590c8f2937a771c8957208ba775b93d5
A8250092 <stanley.mei08@gmail.com>
2023-05-22 17:28:16 +0800

    Update Out of vocabulary words.py

--- Natural Language Processing with Probabilistic Models/week3/Out of vocabulary words.py
+++ Natural Language Processing with Probabilistic Models/week3/Out of vocabulary words.py
@@ -1,6 +1,11 @@
 from collections import Counter
 
 
+def add_k_smoothing_probability(k,vocabulary_size,n_gram_count,n_gram_prefix_count):
+    numerator=n_gram_count+k
+    denominator=n_gram_prefix_count+k*vocabulary_size
+    return numerator/denominator
+
 
 # the target size of the vocabulary
 M=3
@@ -36,3 +41,54 @@ f=3
 for word,freq in word_counts.items():
     if freq==f:
         print(word)
+
+
+# many <unk> low perplexity
+training_set=['i','am','happy','because','i','am','learning','.']
+training_set_unk=['i','am','<UNK>','<UNK>','i','am','<UNK>','<UNK>']
+
+test_set=['i','am','learning']
+test_set_unk=['i','am','<UNK>']
+
+M=len(test_set)
+probability=1
+probability_unk=1
+
+# pre-calculated probabilities
+bigram_probabilities={('i','am'):1.0,('am','happy'):0.5,('happy','because'):1.0,('because','i'):1.0,('am','learning'):0.5,('learning','.'):1.0}
+bigram_probabilities_unk={('i','am'):1.0,('am','<UNK>'):1.0,('<UNK>','<UNK>'):0.5,('<UNK>','i'):0.25}
+
+
+# got through the test set and calculate its bigram probability
+for i in range(len(test_set)-1):
+    bigram=tuple(test_set[i:i+2])
+    probability=probability*bigram_probabilities[bigram]
+    bigram_unk=tuple(training_set_unk[i:i+2])
+    probability_unk=probability_unk*bigram_probabilities_unk[bigram_unk]
+
+# calculate perplexity for both original test set and test set with <UNK>
+perplexity=probability**(-1/M)
+perplexity_unk=probability_unk**(-1/M)
+
+print(f'perplexity for the training set:{perplexity}')
+print(f'perplexity for the training set with <UNK>:{perplexity_unk}')
+
+
+trigram_probabilities={('i','am','happy'):2}
+bigram_probabilities={('am','happy'):10}
+vocabulary_size=5
+k=1
+
+
+probability_known_trigram=add_k_smoothing_probability(k,
+                                                      vocabulary_size,
+                                                      trigram_probabilities[('i','am','happy')],
+                                                      bigram_probabilities[('am','happy')])
+
+probability_unknown_trigram=add_k_smoothing_probability(k,
+                                                        vocabulary_size,
+                                                        0,
+                                                        0)
+
+print(f'probability_known_trigram: {probability_known_trigram}')
+print(f'probability_unknown_trigram: {probability_unknown_trigram}')
