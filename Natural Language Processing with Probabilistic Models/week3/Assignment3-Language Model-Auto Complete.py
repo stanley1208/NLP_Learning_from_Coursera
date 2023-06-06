24f553b5d172678ff97f557dc131eef9735ab02a
A8250092 <stanley.mei08@gmail.com>
2023-06-06 16:03:10 +0800

    Update Assignment3-Language Model-Auto Complete.py

--- Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
+++ Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
@@ -53,3 +53,42 @@ I have a pen.\nI have an apple. \nAh\nApple pen.\n
 print(x)
 
 print(split_to_sentences(x))
+
+
+# UNIT TEST COMMENT: Candidate for Table Driven Tests
+### UNQ_C2 GRADED_FUNCTION: tokenize_sentences ###
+def tokenize_sentences(sentences):
+    """
+    Tokenize sentences into tokens (words)
+
+    Args:
+        sentences: List of strings
+
+    Returns:
+        List of lists of tokens
+    """
+
+    # Initialize the list of lists of tokenized sentences
+    tokenized_sentences = []
+    ### START CODE HERE ###
+
+    # Go through each sentence
+    for sentence in sentences:  # complete this line
+
+        # Convert to lowercase letters
+        sentence = sentence.lower()
+
+        # Convert into a list of words
+        tokenized = nltk.word_tokenize(sentence)
+
+        # append the list of words to the list of lists
+        tokenized_sentences.append(tokenized)
+
+    ### END CODE HERE ###
+
+    return tokenized_sentences
+
+
+# test your code
+sentences = ["Sky is blue.", "Leaves are green.", "Roses are red."]
+print(tokenize_sentences(sentences))
