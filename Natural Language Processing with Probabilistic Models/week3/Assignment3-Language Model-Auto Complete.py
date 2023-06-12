80fb4522d00b80a223efe7d5f8e1b6b0b21ab025
A8250092 <stanley.mei08@gmail.com>
2023-06-12 18:19:36 +0800

    Update Assignment3-Language Model-Auto Complete.py

--- Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
+++ Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
@@ -92,3 +92,29 @@ def tokenize_sentences(sentences):
 # test your code
 sentences = ["Sky is blue.", "Leaves are green.", "Roses are red."]
 print(tokenize_sentences(sentences))
+
+
+
+# UNIT TEST COMMENT: Candidate for Table Driven Tests
+### UNQ_C3 GRADED_FUNCTION: get_tokenized_data ###
+def get_tokenized_data(data):
+    """
+    Make a list of tokenized sentences
+
+    Args:
+        data: String
+
+    Returns:
+        List of lists of tokens
+    """
+    ### START CODE HERE ###
+
+    # Get the sentences by splitting up the data
+    sentences = split_to_sentences(data)
+
+    # Get the list of lists of tokens by tokenizing the sentences
+    tokenized_sentences = tokenize_sentences(sentences)
+
+    ### END CODE HERE ###
+
+    return tokenized_sentences
