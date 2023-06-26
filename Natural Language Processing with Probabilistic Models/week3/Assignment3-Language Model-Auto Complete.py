6094a50389aa4438b7a16dffcb02e708be6a1429
A8250092 <stanley.mei08@gmail.com>
2023-06-26 14:02:32 +0800

    Update Assignment3-Language Model-Auto Complete.py

--- Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
+++ Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
@@ -233,6 +233,7 @@ print(f"Closed vocabulary:")
 print(tmp_closed_vocab)
 
 
+
 # UNIT TEST COMMENT: Candidate for Table Driven Tests
 ### UNQ_C6 GRADED_FUNCTION: replace_oov_words_by_unk ###
 def replace_oov_words_by_unk(tokenized_sentences, vocabulary, unknown_token="<unk>"):
@@ -287,3 +288,76 @@ print(f"Original sentence:")
 print(tokenized_sentences)
 print(f"tokenized_sentences with less frequent words converted to '<unk>':")
 print(tmp_replaced_tokenized_sentences)
+
+
+# UNIT TEST COMMENT: Candidate for Table Driven Tests
+### UNQ_C7 GRADED_FUNCTION: preprocess_data ###
+def preprocess_data(train_data, test_data, count_threshold, unknown_token="<unk>",
+                    get_words_with_nplus_frequency=get_words_with_nplus_frequency,
+                    replace_oov_words_by_unk=replace_oov_words_by_unk):
+    """
+    Preprocess data, i.e.,
+        - Find tokens that appear at least N times in the training data.
+        - Replace tokens that appear less than N times by "<unk>" both for training and test data.
+    Args:
+        train_data, test_data: List of lists of strings.
+        count_threshold: Words whose count is less than this are
+                      treated as unknown.
+
+    Returns:
+        Tuple of
+        - training data with low frequent words replaced by "<unk>"
+        - test data with low frequent words replaced by "<unk>"
+        - vocabulary of words that appear n times or more in the training data
+    """
+    ### START CODE HERE ###
+
+    # Get the closed vocabulary using the train data
+    vocabulary = get_words_with_nplus_frequency(train_data,count_threshold)
+
+    # For the train data, replace less common words with "<unk>"
+    train_data_replaced = replace_oov_words_by_unk(train_data,vocabulary,unknown_token)
+
+    # For the test data, replace less common words with "<unk>"
+    test_data_replaced = replace_oov_words_by_unk(test_data,vocabulary,unknown_token)
+
+    ### END CODE HERE ###
+    return train_data_replaced, test_data_replaced, vocabulary
+
+
+
+# test your code
+tmp_train = [['sky', 'is', 'blue', '.'],
+     ['leaves', 'are', 'green']]
+tmp_test = [['roses', 'are', 'red', '.']]
+
+tmp_train_repl, tmp_test_repl, tmp_vocab = preprocess_data(tmp_train,
+                                                           tmp_test,
+                                                           count_threshold = 1
+                                                          )
+
+print("tmp_train_repl")
+print(tmp_train_repl)
+print()
+print("tmp_test_repl")
+print(tmp_test_repl)
+print()
+print("tmp_vocab")
+print(tmp_vocab)
+
+
+minimum_freq = 2
+train_data_processed, test_data_processed, vocabulary = preprocess_data(train_data,
+                                                                        test_data,
+                                                                        minimum_freq)
+
+print("First preprocessed training sample:")
+print(train_data_processed[0])
+print()
+print("First preprocessed test sample:")
+print(test_data_processed[0])
+print()
+print("First 10 vocabulary:")
+print(vocabulary[0:10])
+print()
+print("Size of vocabulary:", len(vocabulary))
