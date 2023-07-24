79c028733ec0eba5f9faae144c80bbdb61947d67
A8250092 <stanley.mei08@gmail.com>
2023-07-24 18:18:20 +0800

    Update Assignment3-Language Model-Auto Complete.py

--- Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
+++ Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
@@ -361,3 +361,56 @@ print("First 10 vocabulary:")
 print(vocabulary[0:10])
 print()
 print("Size of vocabulary:", len(vocabulary))
+
+
+# UNIT TEST COMMENT: Candidate for Table Driven Tests
+### UNQ_C8 GRADED FUNCTION: count_n_grams ###
+def count_n_grams(data, n, start_token='<s>', end_token='<e>'):
+    """
+    Count all n-grams in the data
+
+    Args:
+        data: List of lists of words
+        n: number of words in a sequence
+
+    Returns:
+        A dictionary that maps a tuple of n-words to its frequency
+    """
+
+    # Initialize dictionary of n-grams and their counts
+    n_grams = {}
+
+    ### START CODE HERE ###
+
+    # Go through each sentence in the data
+    for sentence in data:  # complete this line
+
+        # prepend start token n times, and  append the end token one time
+        sentence = [start_token]*n+sentence+[end_token]
+
+        # convert list to tuple
+        # So that the sequence of words can be used as
+        # a key in the dictionary
+        sentence = tuple(sentence)
+
+        # Use 'i' to indicate the start of the n-gram
+        # from index 0
+        # to the last index where the end of the n-gram
+        # is within the sentence.
+        m = len(sentence) if n == 1 else len(sentence) - 1
+        for i in range(m):  # complete this line
+
+            # Get the n-gram from i to i+n
+            n_gram = sentence[i:i+n]
+
+            # check if the n-gram is in the dictionary
+            if n_gram in n_grams.keys():  # complete this line with the proper condition
+
+                # Increment the count for this n-gram
+                n_grams[n_gram] += 1
+            else:
+                # Initialize this n-gram count to 1
+                n_grams[n_gram] = 1
+
+            ### END CODE HERE ###
+    return n_grams
