6ce97bd68ff69f1eb2c3201f88551a256c55cdb2
A8250092 <stanley.mei08@gmail.com>
2023-07-25 17:12:34 +0800

    Update Assignment3-Language Model-Auto Complete.py

--- Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
+++ Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
@@ -414,3 +414,67 @@ def count_n_grams(data, n, start_token='<s>', end_token='<e>'):
 
             ### END CODE HERE ###
     return n_grams
+
+
+### UNQ_C9 GRADED FUNCTION: estimate_probability ###
+def estimate_probability(word, previous_n_gram,
+                         n_gram_counts, n_plus1_gram_counts, vocabulary_size, k=1.0):
+    """
+    Estimate the probabilities of a next word using the n-gram counts with k-smoothing
+
+    Args:
+        word: next word
+        previous_n_gram: A sequence of words of length n
+        n_gram_counts: Dictionary of counts of n-grams
+        n_plus1_gram_counts: Dictionary of counts of (n+1)-grams
+        vocabulary_size: number of words in the vocabulary
+        k: positive constant, smoothing parameter
+
+    Returns:
+        A probability
+    """
+    # convert list to tuple to use it as a dictionary key
+    previous_n_gram = tuple(previous_n_gram)
+
+    ### START CODE HERE ###
+
+    # Set the denominator
+    # If the previous n-gram exists in the dictionary of n-gram counts,
+    # Get its count.  Otherwise set the count to zero
+    # Use the dictionary that has counts for n-grams
+    previous_n_gram_count = n_gram_counts[previous_n_gram] if previous_n_gram in n_gram_counts else 0
+
+    # Calculate the denominator using the count of the previous n gram
+    # and apply k-smoothing
+    denominator = previous_n_gram_count+k*vocabulary_size
+
+    # Define n plus 1 gram as the previous n-gram plus the current word as a tuple
+    n_plus1_gram = previous_n_gram+(word,)
+
+    # Set the count to the count in the dictionary,
+    # otherwise 0 if not in the dictionary
+    # use the dictionary that has counts for the n-gram plus current word
+    n_plus1_gram_count = n_plus1_gram_counts[n_plus1_gram] if n_plus1_gram in n_plus1_gram_counts else 0
+
+    # Define the numerator use the count of the n-gram plus current word,
+    # and apply smoothing
+    numerator = n_plus1_gram_count+k
+
+    # Calculate the probability as the numerator divided by denominator
+    probability = numerator/denominator
+
+    ### END CODE HERE ###
+
+    return probability
+
+
+# test your code
+sentences = [['i', 'like', 'a', 'cat'],
+             ['this', 'dog', 'is', 'like', 'a', 'cat']]
+unique_words = list(set(sentences[0] + sentences[1]))
+
+unigram_counts = count_n_grams(sentences, 1)
+bigram_counts = count_n_grams(sentences, 2)
+tmp_prob = estimate_probability("cat", ["a"], unigram_counts, bigram_counts, len(unique_words), k=1)
+
+print(f"The estimated probability of word 'cat' given the previous n-gram 'a' is: {tmp_prob:.4f}")
