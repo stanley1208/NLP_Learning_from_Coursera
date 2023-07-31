3364b33f759c391add88a91b63af8943aa83438d
A8250092 <stanley.mei08@gmail.com>
2023-07-31 18:19:24 +0800

    Update Assignment3-Language Model-Auto Complete.py

--- Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
+++ Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
@@ -673,3 +673,79 @@ perplexity_test = calculate_perplexity(test_sentence,
                                        unigram_counts, bigram_counts,
                                        len(unique_words), k=1.0)
 print(f"Perplexity for test sample: {perplexity_test:.4f}")
+
+
+# UNQ_C11 GRADED FUNCTION: suggest_a_word
+def suggest_a_word(previous_tokens, n_gram_counts, n_plus1_gram_counts, vocabulary, end_token='<e>',
+                   unknown_token="<unk>", k=1.0, start_with=None):
+    """
+    Get suggestion for the next word
+
+    Args:
+        previous_tokens: The sentence you input where each token is a word. Must have length >= n
+        n_gram_counts: Dictionary of counts of n-grams
+        n_plus1_gram_counts: Dictionary of counts of (n+1)-grams
+        vocabulary: List of words
+        k: positive constant, smoothing parameter
+        start_with: If not None, specifies the first few letters of the next word
+
+    Returns:
+        A tuple of
+          - string of the most likely next word
+          - corresponding probability
+    """
+
+    # length of previous words
+    n = len(list(n_gram_counts.keys())[0])
+
+    # append "start token" on "previous_tokens"
+    previous_tokens = ['<s>'] * n + previous_tokens
+
+    # From the words that the user already typed
+    # get the most recent 'n' words as the previous n-gram
+    previous_n_gram = previous_tokens[-n:]
+
+    # Estimate the probabilities that each word in the vocabulary
+    # is the next word,
+    # given the previous n-gram, the dictionary of n-gram counts,
+    # the dictionary of n plus 1 gram counts, and the smoothing constant
+    probabilities = estimate_probabilities(previous_n_gram,
+                                           n_gram_counts, n_plus1_gram_counts,
+                                           vocabulary, k=k)
+
+    # Initialize suggested word to None
+    # This will be set to the word with highest probability
+    suggestion = None
+
+    # Initialize the highest word probability to 0
+    # this will be set to the highest probability
+    # of all words to be suggested
+    max_prob = 0
+
+    ### START CODE HERE ###
+
+    # For each word and its probability in the probabilities dictionary:
+    for word, prob in probabilities.items():  # complete this line
+
+        # If the optional start_with string is set
+        if start_with!=None:  # complete this line with the proper condition
+
+            # Check if the beginning of word does not match with the letters in 'start_with'
+            if not word.startswith(start_with):  # complete this line with the proper condition
+
+                # if they don't match, skip this word (move onto the next word)
+                continue
+
+        # Check if this word's probability
+        # is greater than the current maximum probability
+        if prob>max_prob:  # complete this line with the proper condition
+
+            # If so, save this word as the best suggestion (so far)
+            suggestion = word
+
+            # Save the new maximum probability
+            max_prob = prob
+
+    ### END CODE HERE
+
+    return suggestion, max_prob
