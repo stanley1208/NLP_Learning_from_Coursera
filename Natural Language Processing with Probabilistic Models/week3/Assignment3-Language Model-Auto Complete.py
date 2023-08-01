f72fe59b2ceccd01e153bf7af743edece3d20dea
A8250092 <stanley.mei08@gmail.com>
2023-08-01 17:42:17 +0800

    Update Assignment3-Language Model-Auto Complete.py

--- Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
+++ Natural Language Processing with Probabilistic Models/week3/Assignment3-Language Model-Auto Complete.py
@@ -749,3 +749,54 @@ def suggest_a_word(previous_tokens, n_gram_counts, n_plus1_gram_counts, vocabula
     ### END CODE HERE
 
     return suggestion, max_prob
+
+# test your code
+sentences = [['i', 'like', 'a', 'cat'],
+             ['this', 'dog', 'is', 'like', 'a', 'cat']]
+unique_words = list(set(sentences[0] + sentences[1]))
+
+unigram_counts = count_n_grams(sentences, 1)
+bigram_counts = count_n_grams(sentences, 2)
+
+previous_tokens = ["i", "like"]
+tmp_suggest1 = suggest_a_word(previous_tokens, unigram_counts, bigram_counts, unique_words, k=1.0)
+print(f"The previous words are 'i like',\n\tand the suggested word is `{tmp_suggest1[0]}` with a probability of {tmp_suggest1[1]:.4f}")
+
+print()
+# test your code when setting the starts_with
+tmp_starts_with = 'c'
+tmp_suggest2 = suggest_a_word(previous_tokens, unigram_counts, bigram_counts, unique_words, k=1.0, start_with=tmp_starts_with)
+print(f"The previous words are 'i like', the suggestion must start with `{tmp_starts_with}`\n\tand the suggested word is `{tmp_suggest2[0]}` with a probability of {tmp_suggest2[1]:.4f}")
+
+
+def get_suggestions(previous_tokens, n_gram_counts_list, vocabulary, k=1.0, start_with=None):
+    model_counts = len(n_gram_counts_list)
+    suggestions = []
+    for i in range(model_counts - 1):
+        n_gram_counts = n_gram_counts_list[i]
+        n_plus1_gram_counts = n_gram_counts_list[i + 1]
+
+        suggestion = suggest_a_word(previous_tokens, n_gram_counts,
+                                    n_plus1_gram_counts, vocabulary,
+                                    k=k, start_with=start_with)
+        suggestions.append(suggestion)
+    return suggestions
+
+
+# test your code
+sentences = [['i', 'like', 'a', 'cat'],
+             ['this', 'dog', 'is', 'like', 'a', 'cat']]
+unique_words = list(set(sentences[0] + sentences[1]))
+
+unigram_counts = count_n_grams(sentences, 1)
+bigram_counts = count_n_grams(sentences, 2)
+trigram_counts = count_n_grams(sentences, 3)
+quadgram_counts = count_n_grams(sentences, 4)
+qintgram_counts = count_n_grams(sentences, 5)
+
+n_gram_counts_list = [unigram_counts, bigram_counts, trigram_counts, quadgram_counts, qintgram_counts]
+previous_tokens = ["i", "like"]
+tmp_suggest3 = get_suggestions(previous_tokens, n_gram_counts_list, unique_words, k=1.0)
+
+print(f"The previous words are 'i like', the suggestions are:")
+print(tmp_suggest3)
