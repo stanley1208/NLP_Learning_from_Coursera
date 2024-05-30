803fbb44d49b804946b1112062fad8f40e7d944c
stanley1208 <stanley.mei08@gmail.com>
2024-05-30 19:15:07 +0800

    update

--- Natural Language Processing with Probabilistic Models/week4/Data Preparation.py
+++ Natural Language Processing with Probabilistic Models/week4/Data Preparation.py
@@ -12,3 +12,16 @@ from utils2 import get_dict
 
 # Define a corpus
 corpus = 'Who ❤️ "word embeddings" in 2020? I do!!!'
+
+# print original corpus
+print(f'Corpus: {corpus}')
+# substitution
+data=re.sub(r'[,!?;-]+','.',corpus)
+# print cleaned corpus
+print(f'Cleaned Corpus: {data}')
+# print cleand corpus
+print(f'Initial string: {data}')
+# tokenize the cleaned corpus
+data=nltk.word_tokenize(data)
+# print tokenize string
+print(f'After tokenization: {data}')
